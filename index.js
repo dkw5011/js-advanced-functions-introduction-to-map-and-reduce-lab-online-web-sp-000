bd7c077b2b8d5753c02dabc759f0a28a8becf3b8
dkw5011 <dkw5011@gmail.com>
2021-03-11 14:49:48 -0500

    commit tree 6c1b52592926b5f1eea8ef71c954e6b3680111f1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+const mapToNegativize = function(sourceArray) {
+    var newArray;
+    newArray = sourceArray.map(myFunction)
+    function myFunction(n){
+      return  n * -1 ;
+    };
+    return newArray
+};
